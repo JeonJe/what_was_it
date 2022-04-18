f0c4a45fa44dc996d7261e5fb2e3c3cd4242a3ba
Jeon Je <whssodi@gmail.com>
2022-04-18 23:13:31 +0900

    게시판 글쓰기 까지

---
+++ webpage/src/main/java/com/sideproject/webpage/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.sideproject.webpage.repository;
+
+import com.sideproject.webpage.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BoardRepository extends JpaRepository<Board, Long>{
+
+
+}
