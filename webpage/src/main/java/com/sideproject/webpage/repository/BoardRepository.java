b064e5e588815739afcf5e9f929676c4b1ea011c
Jeon Je <whssodi@gmail.com>
2022-07-11 22:52:14 +0900

    paging and search with JPA

--- webpage/src/main/java/com/sideproject/webpage/repository/BoardRepository.java
+++ webpage/src/main/java/com/sideproject/webpage/repository/BoardRepository.java
@@ -1,10 +1,18 @@
 package com.sideproject.webpage.repository;
 
 import com.sideproject.webpage.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long>{
 
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title,String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
